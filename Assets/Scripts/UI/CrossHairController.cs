652f2ef5858207447f0107e288b6fb3e6ddc6e49
PetrZavodny <petr.zavodny@chyronhego.com>
2020-09-02 11:08:08 +0200

    UI MVP ready - Before Cleanup

---
+++ Assets/Scripts/UI/CrossHairController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
+
+//Fireball Games * * * PetrZavodny.com
+
+public class CrossHairController : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private Sprite freeFlightSprite;
+    [SerializeField] private Sprite BuildSprite;
+    [SerializeField] private Sprite DestroySprite;
+    [SerializeField] private float freeFlightIconScale = 1f;
+    [SerializeField] private float buildIconScale = 0.8f;
+    [SerializeField] private float destroyIconScale = 0.8f;
+    [SerializeField] private Image imagePivot;
+    [SerializeField] private Animator animator;
+    private GameMode gameMode;
+#pragma warning restore 649
+
+    /// <summary>
+    /// Used in GameController
+    /// </summary>
+    /// <param name="newMode"></param>
+    public void SetCrossHair(GameMode newMode)
+    {
+        gameMode = newMode;
+        
+        switch (newMode)
+        {
+            case GameMode.FreeFlight:
+                SetForFreeFlightMode(); break;
+            case GameMode.Build:
+                SetForBuildMode(); break;
+            case GameMode.Destroy:
+                SetDestroySprite(); break;
+            default: SetFreeFlightSprite();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Run from animator
+    /// </summary>
+    private void SetForFreeFlightMode()
+    {
+        animator.SetBool(Strings.AtTheTop, true);
+    }
+
+    private void SetFreeFlightSprite()
+    {
+        SetImagePivotScale(freeFlightIconScale);
+        imagePivot.sprite = freeFlightSprite;
+    }
+
+    private void SetForBuildMode()
+    {
+        animator.SetBool(Strings.AtTheTop, false);
+    }
+
+    /// <summary>
+    /// Run from animator
+    /// </summary>
+    private void SetBuildSprite()
+    {
+        if (gameMode == GameMode.Build)
+        {
+            SetImagePivotScale(buildIconScale);
+            imagePivot.sprite = BuildSprite;
+        }
+    }
+
+    private void SetDestroySprite()
+    {
+        SetImagePivotScale(destroyIconScale);
+        imagePivot.sprite = DestroySprite;
+    }
+
+    private void SetImagePivotScale(float newScale)
+    {
+        imagePivot.transform.localScale = new Vector3(newScale, newScale, newScale);
+    }
+}
